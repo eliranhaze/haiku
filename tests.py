fced80d36619d2a30156bd4dc244bca8c057d450
Eliran Haziza <eliranhaze@gmail.com>
2019-09-20 10:35:15 -0400

    added more tests

--- tests.py
+++ tests.py
@@ -6,6 +6,12 @@ haiku1 = 'snow falls and is white the falling is a process the whiteness is not.
 haiku2 = "how many persons entered the duplication center yesterday? Gee I don't know!"
 not_haiku1 = 'snow does not fall at all and is white the falling is a process the whiteness is not. Not at all.'
 not_haiku2 = 'a process the whiteness is not.'
+haiku_part1 = 'snow falls and is white.'
+haiku_part2 = 'the falling is a process.'
+haiku_part3 = 'the whiteness is not.'
+
+def concat(*args):
+    return ' '.join(args)
 
 class TestDetection(unittest.TestCase):
 
@@ -16,6 +22,19 @@ class TestDetection(unittest.TestCase):
     def test_negative(self):
         self.assertEqual(len(extract_haikus(not_haiku1)), 0)
         self.assertEqual(len(extract_haikus(not_haiku2)), 0)
+
+    def test_multiple(self):
+        self.assertEqual(len(extract_haikus(concat(haiku1, haiku2))), 2)
+        self.assertEqual(len(extract_haikus(concat(haiku1, haiku2, haiku1, haiku2))), 4)
+        self.assertEqual(len(extract_haikus(concat(haiku1, not_haiku1))), 1)
+        self.assertEqual(len(extract_haikus(concat(haiku1, not_haiku1, haiku2))), 2)
+        self.assertEqual(len(extract_haikus(concat(not_haiku2, not_haiku1))), 0)
+        self.assertEqual(len(extract_haikus(concat(not_haiku2, not_haiku1, not_haiku1, haiku2))), 1)
+
+    def test_multipart(self):
+        self.assertEqual(len(extract_haikus(concat(haiku_part1, haiku_part2))), 0)
+        self.assertEqual(len(extract_haikus(concat(haiku_part1, haiku_part2, haiku_part3))), 1)
+        self.assertEqual(len(extract_haikus(concat(haiku_part1, haiku_part2, haiku_part3, haiku2))), 2)
 
 if __name__ == '__main__':
     unittest.main()
