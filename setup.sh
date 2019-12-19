9d3724012499acc7389cc45918c7d19f4b2f38fe
Eliran Haziza <eliranhaze@gmail.com>
2019-12-18 19:46:12 -0500

    setup script fot playing with pdfs

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "missing argument"
+    exit 0
+fi
+
+echo "clearing existing txt files"
+rm -f tmp/*txt
+echo "copying from $1"
+cp ~/Downloads/journal-papers/$1/*pdf tmp/
+
+echo "converting pdf to txt"
+cd tmp
+for file in *.pdf; do pdftotext "$file" "$file.txt"; done
+rm -f *.pdf
+cd ..
+
+echo "done"
